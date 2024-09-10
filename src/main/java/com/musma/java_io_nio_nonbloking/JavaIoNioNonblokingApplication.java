9859590e9197eeb374e1f4e0d935e7d0ab8e7b83
hyuk12 <hotman7025@naver.com>
2024-09-10 10:39:18 +0900

    init: 초기 프로젝트

---
+++ src/main/java/com/musma/java_io_nio_nonbloking/JavaIoNioNonblokingApplication.java
@@ -0,0 +1,13 @@
+package com.musma.java_io_nio_nonbloking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIoNioNonblokingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIoNioNonblokingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musma/java_io_nio_nonbloking/JavaIoNioNonblokingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musma.java_io_nio_nonbloking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaIoNioNonblokingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
